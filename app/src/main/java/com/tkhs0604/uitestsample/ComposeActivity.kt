b2d9d3ad2d754cd25c17afac52f6c86ca8237296
kenken <43767445+tkhs0604@users.noreply.github.com>
2023-09-01 22:11:49 +0900

    update theme

--- app/src/main/java/com/tkhs0604/uitestsample/ComposeActivity.kt
+++ app/src/main/java/com/tkhs0604/uitestsample/ComposeActivity.kt
@@ -14,13 +14,11 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
