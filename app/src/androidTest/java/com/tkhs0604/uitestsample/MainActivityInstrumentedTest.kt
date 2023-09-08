468c3fe368774fc928da48991225977b6e91f8ce
kenken <43767445+tkhs0604@users.noreply.github.com>
2023-09-09 01:07:44 +0900

    remove an unit test

--- app/src/androidTest/java/com/tkhs0604/uitestsample/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/tkhs0604/uitestsample/MainActivityInstrumentedTest.kt
@@ -1,12 +1,6 @@
 package com.tkhs0604.uitestsample
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
 import com.tkhs0604.uitestsample.featureflag.FeatureFlagProvider
 import com.tkhs0604.uitestsample.page.MainPage
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -36,18 +30,6 @@ class MainActivityInstrumentedTest {
 
     @Test
     fun navigationTest() {
-        composeTestRule.onNodeWithText("Go to next activity").performClick()
-
-        if (featureFlagProvider.shouldUseComposeScreen()) {
-            composeTestRule.onNodeWithText("ComposeActivity").assertIsDisplayed()
-        } else {
-            Espresso.onView(ViewMatchers.withText("AndroidViewActivity"))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        }
-    }
-
-    @Test
-    fun navigationTestByUsingPageObject() {
         MainPage
             .clickButton(featureFlagProvider.shouldUseComposeScreen())
             .assertScreenName()
